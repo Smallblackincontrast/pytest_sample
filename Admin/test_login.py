6f284de77f090062586da163d25162b7b67dbdf1
ruanzhe <ruanzhe666@163.com>
2019-03-13 14:52:30 +0800

    update

--- Admin/test_login.py
+++ Admin/test_login.py
@@ -2,35 +2,33 @@
 # -*- coding: utf-8 -*-
 # @File  : test_login.py
 # @Author  : Ruanzhe
-# @Date  : 2019/2/12  11:16
-
-from Tools.web_flow_test_case import WebFlowTestCase
+# @Date  : 2019/3/13  10:10
 
 
-class LineBusAdminLogin(WebFlowTestCase):
+import time
+from selenium import webdriver
 
-	def setUp(self):
-		self.url = "http://line-qa.ubtbus.top:8079/#/login"
-		self.username = "input[sid='usernameInput']"
-		self.password = "input[sid='passwordInput']"
-		self.loginbutton = "button[sid='loginBtn']"
 
-	def _click_login(self):
-		self.click(self.loginbutton)
+driver = webdriver.Chrome()
+driver.get("http://ticketing-qa.ubtbus.top/#/login")
+driver.maximize_window()
+time.sleep(1)
+username = driver.find_element_by_css_selector('input[sid="usernameInput"]')
+username.send_keys("qa_ruanzhe")
+password = driver.find_element_by_css_selector('input[sid="passwordInput"]')
+password.send_keys("666666")
+loginbutton = driver.find_element_by_css_selector('button[sid="loginBtn"]')
+loginbutton.click()
+time.sleep(3)
+message = driver.find_element_by_css_selector('div[class="wel_title"] > i:first-child').text
+print(message)
+try:
+	assert message == "Hi, qa_ruanzhe"
+	print("Test Pass!")
+except Exception as e:
+	print("Test fail.", format(e))
+finally:
+	driver.quit()
 
-	def _send_username(self, username):
-		self.send_keys(self.username, username)
 
-	def _send_password(self, password):
-		self.send_keys(self.password, password)
 
-	def admin_login_flow(self, username, password):
-		self.open(self.url, is_active_window=True)
-		self._send_username(username)
-		self._send_password(password)
-		self._click_login()
-
-	def test_login_success(self):
-		self.admin_login_flow('LS_admin', '666666')
-		self.keep()
-		self.assertEqual()
